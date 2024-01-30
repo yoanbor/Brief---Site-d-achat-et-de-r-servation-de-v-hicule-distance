0592c1c897e7d5f3cc04a3fb0cc889e6fadbc238
Sarah-Katz <Sarahkatz59@gmail.com>
2024-01-30 13:52:40 +0100

    feat(DB): Added db connection

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
+require("dotenv").config({ path: "./" + ".env" });
 const express = require("express");
+const pool = require("./config/db.config.js");
+
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+//Functions
+const getReservation = (req, res) => {
+    pool.query("SELECT * FROM reservation", (error, reservation) => {
+        if (error) {
+            throw error;
+        }
+        res.status(200).json(reservation.rows);
+    });
+};
+
+//Here you can add your routes
+//Here's an example
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.get("/reservations", getReservation);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on the port  ${PORT}`);
+});
